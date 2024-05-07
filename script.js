392332a309bcee9fbac3933182ff499c61df0803
zpdh <36316038+zpdh@users.noreply.github.com>
2024-05-07 20:04:10 -0300

    Fix being able to place more than 1 decimal

--- script.js
+++ script.js
@@ -23,11 +23,13 @@ const opBtns = document.querySelectorAll('.opbtn')
 const clearBtn = document.getElementsByClassName('clearbtn')
 const equalBtn = document.getElementsByClassName('equals')
 const output = document.getElementById('output')
-
+const decimal = document.getElementById('decimal')
 
 numBtns.forEach(btn => btn.addEventListener('click', () => updateOutput(btn.textContent)))
 
 opBtns.forEach(btn => btn.addEventListener('click', () => inputOperator(btn.textContent)))
+
+decimal.addEventListener('click', () => inputDecimal())
 
 clearBtn[0].addEventListener('click', () => clear())
 
@@ -35,19 +37,40 @@ equalBtn[0].addEventListener('click', () => calculate())
 
 
 function updateOutput(num) {
-    if (output.textContent == 'ㅤ' || output.textContent == 'NaN' || output.textContent == 'Error') {
+    if (output.textContent == 'ㅤ' || output.textContent == 'NaN' || output.textContent == 'Infinity') {
         output.textContent = ''
         output.textContent += num
     } else output.textContent += num
+    if (operator == 'none') firstNum = output.textContent
+    if (operator !== 'none') secondNum = output.textContent.split(operator).pop()
 }
 
 function inputOperator(operation) {
     if (operator !== 'none') return
     else {
+    firstNum = output.textContent
     output.textContent += ` ${operation} `
     operator = `${operation}`
 }} 
 
+function inputDecimal() {
+    if (operator == 'none') {
+    if (firstNum.includes('.')) return
+
+    else {
+        firstNum += '.'
+        output.textContent += '.'
+
+    }} if (operator !== 'none') {
+    if (secondNum.includes('.'))  return
+
+    else {
+        secondNum += '.'
+        output.textContent += '.'
+
+    }}
+
+}
 function clear() {
     output.textContent = 'ㅤ'
     firstNum = ''
@@ -57,22 +80,10 @@ function clear() {
 
 function calculate() {
     if (output.textContent == 'ㅤ' || operator == 'none') return
-    else {
-    let operationContent = output.innerHTML
-    operationContent = operationContent.split(' ')
-
-    firstNum = operationContent[0]
-    operator = operationContent[1]
-    secondNum = operationContent[2]
-
-    if (operationContent[1] == '÷' && operationContent[2] == '0') output.textContent = 'Error'
-
-    else {
     output.textContent = Math.round(
         operate(firstNum, secondNum, operator
         ) * 100) / 100
-    }
     firstNum = ''
     secondNum = ''
     operator = 'none'
-}}
+}
